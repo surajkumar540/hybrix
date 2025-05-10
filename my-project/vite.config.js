b13f5135dbc60699ff8ac7695e0ac1ccf2a62f1b
suraj <surajgautam56878@gmail.com>
2025-05-10 23:48:57 +0530

    add vercel and vite config

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Make sure this is correct
   plugins: [react()],
   server: {
     historyApiFallback: true, // For react-router to handle routing
